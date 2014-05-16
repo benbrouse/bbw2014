921cb82824240923766dd4f8fff4781847ea0366
Ben Brouse <ben@brouse.net>
2014-05-16 15:56:13 -0400

    reworked event detail as a modal dialog

--- www/js/events/event-index-controller.js
+++ www/js/events/event-index-controller.js
@@ -1,33 +1,89 @@
 ﻿angular.module('bbw.event-index-controller', ['ionic'])
 
 // A simple controller that fetches a list of data from a service
-.controller('EventIndexCtrl', ['$scope', '$log', '$filter', '$ionicModal', 'LoaderService', 'EventsService', function($scope, $log, $filter, $ionicModal, LoaderService, EventsService) {
+.controller('EventIndexCtrl', ['$scope', '$log', '$filter', '$ionicModal', 'LoaderService', 'EventsService', 'AddressService', 'GoogleMapsService', function($scope, $log, $filter, $ionicModal, LoaderService, EventsService, AddressService, GoogleMapsService) {
     $scope.initialized = false;
+
+    var getMapInstance = function () {
+        var mapEl = angular.element(document.querySelector('.angular-google-map'));
+        var iScope = mapEl.isolateScope();
+        var map = iScope.map;
+
+        return map;
+    };
 
     // Load the modal from the given template URL
     $ionicModal.fromTemplateUrl('templates/event-filter.html', {
         scope: $scope,
         animation: 'slide-in-up'
     }).then(function(modal) {
-        $scope.modal = modal;
+        $scope.modalFilter = modal;
     });
+
+    // Load the modal from the given template URL
+    $ionicModal.fromTemplateUrl('templates/event-detail2.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modalEvent = modal;
+    });
+
 
     $scope.toggle = function() {
         //alert('Test');
     };
 
-    $scope.openModal = function() {
-        $scope.modal.show();
+    $scope.openFilterModal = function () {
+        $scope.modalFilter.show();
     };
 
-    $scope.closeModal = function() {
-        $scope.modal.hide();
+    $scope.closeFilterModal = function() {
+        $scope.modalFilter.hide();
+    };
+
+     
+    $scope.openEventModal = function (eventId) {
+        $scope.event = EventsService.get(eventId);
+        $scope.currentModal = "eventDetail";
+        $scope.modalEvent.show();
+    };
+
+    $scope.closeEventModal = function () {
+        $scope.modalEvent.hide();
+        $scope.currentModal = null;
     };
 
     //Be sure to cleanup the modal
     $scope.$on('$destroy', function() {
-        $scope.modal.remove();
+        $scope.modalFilter.remove();
+        $scope.modalEvent.remove();
     });
+
+    // Execute action on hide modal
+    $scope.$on('modal.shown', function (modal) {
+        if ($scope.currentModal == "eventDetail") {
+            // todo: change the map location
+            initializeMap();
+
+            var eventAddress = $scope.event.location.address;
+            if (!angular.isUndefined(eventAddress) && angular.isString(eventAddress)) {
+                AddressService.geocode(eventAddress).then(function (location) {
+                    //$scope.map.event.trigger($scope.map, "resize");
+                    $scope.map.setZoom(16);
+                    $scope.map.setCenter(location);
+                });
+            }
+        }
+    });
+
+    $scope.map = {
+        center: {
+            latitude: 45,
+            longitude: -73
+        },
+        bounds: {},
+        zoom: 8
+    };
 
     // Show loader from service
     LoaderService.show('Retrieving Event List');
@@ -61,4 +117,24 @@
         $log.write(reason);
     });
 
+    var initializeMap = function ()
+    {
+        var mapOptions = {
+            center: new google.maps.LatLng(43.07493, -89.381388),
+            zoom: 16,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        var mapElement = document.getElementById('map');
+        var map = new google.maps.Map(mapElement, mapOptions);
+
+        // Stop the side bar from dragging when mousedown/tapdown on the map
+        var mapElement = document.getElementById('map');
+        google.maps.event.addDomListener(mapElement, 'mousedown', function (e) {
+            e.preventDefault();
+            return false;
+        });
+
+        $scope.map = map;
+    }
 }]);
